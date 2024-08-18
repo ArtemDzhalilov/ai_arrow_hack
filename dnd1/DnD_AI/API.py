55e6ea88608c2524b6849ce8c85537bf2d2970a1
ArtemDzhalilov <88921907+ArtemDzhalilov@users.noreply.github.com>
2024-08-18 11:51:53 +0300

    Update API.py

--- dnd1/DnD_AI/API.py
+++ dnd1/DnD_AI/API.py
@@ -1,3 +1,3 @@
 hf_api_key = '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'
-openai_api_key = 'sk-M6nTKyYUrj_wB7u8VCZMAb4kP6ErJ0s8sxlh9Iu4xQT3BlbkFJ-MHp0LwkJT11Fbfteppyy2B3lRX4x9NfMD20bjT-cA'
+openai_api_key = ''
 gemini_api_key = ''
